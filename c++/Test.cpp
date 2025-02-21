203059c667946be97560dc82e2f560df11e89217
SILVAJESUS2005 <159391539+SILVAJESUS2005@users.noreply.github.com>
2025-02-20 22:20:43 -0600

    Respositorio inicial local ICPC

---
+++ c++/Test.cpp
@@ -0,0 +1,22 @@
+/*
+* ITESS - TICS - 401
+* ICPC - JLS
+* Compilación c++ y ejecucion CMD
+* 20-Febrero-2025
+* jesuslopezsilva206@gmail.com
+*
+* ----------------------PASOS PARA COMPILAR DESDE CMD UN ARCHIVO-----------------------------
+* PRIMER PASO: Copiar ruta donde esta la carpeta de los archivos: cd C:\2025_ICPC_coding_cup\c++
+* SEGUNDO PASO (Opcional): Se usa para visualizar que se encuentra en esa carpeta: dir
+* TERCER PASO: Compilas como el siguiente ejemplo: c++ (Test aqui va el nombre del archivo en este caso Test).cpp -o (Test aqui va el nombre del archivo en este caso Test).exe
+* CUARTO PASO: Ejecutas como el siguiente ejemplo: Test(Nombre del archivo ya compilado).exe
+* ----------------- LISTO!!! LA CONSOLA TE ARROJA EL RESULTADO ------------------------------
+*/
+
+#include <iostream>
+
+using namespace std;
+int main()
+{
+    std::cout << "Hello, programmers" << std::endl;  
+}
